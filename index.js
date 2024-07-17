304fe12602a7b676bb5d8266c5737e7f3f7ac6ce
fxhmyz <alifahmy99111@gmail.com>
2024-07-17 15:29:37 +0300

    first initial commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Using reduce() to sum up all the batteries
+const totalBatteries = batteryBatches.reduce((accumulator, currentBatch) => {
+    return accumulator + currentBatch;
+  }, 0); // Initial value of accumulator is set to 0
+  
+  console.log(totalBatteries); // Output: 31
